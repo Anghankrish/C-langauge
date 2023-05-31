88d657ddb62b2aad83b2afe5f0818124253afe3b
Anghankrish <134935345+Anghankrish@users.noreply.github.com>
2023-05-31 18:39:12 +0530

    Add files via upload

---
+++ day-2/day-2__2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+  int main()
+  {
+   printf(" - - - - - - \n");
+   printf("k            |\n");
+   printf("r            |\n");
+   printf("i            |\n");
+   printf("s            |\n");
+   printf("h            |\n");
+   printf(" - - - - - - \n");
+   \
+   return 0;
+  }
---
+++ day-2/day-2__3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+  int main()
+  {
+   printf(" krish \n");
+   printf("18\n");
+   printf("ashadeep iit\n");
+  
+   return 0;
+  }
