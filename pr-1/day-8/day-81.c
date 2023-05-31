57e6ac92d3db6c8e7bfd26b722679aae9a443958
Anghankrish <134935345+Anghankrish@users.noreply.github.com>
2023-05-31 18:46:21 +0530

    Add files via upload

---
+++ pr-1/day-8/day-81.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b;
+	
+	printf("enter first number :");
+	scanf("%d",&a);
+	
+	printf("enter second number :");
+	scanf("%d",&b);
+	
+	if(a<b)
+	{
+		printf("a is smaller number ");
+	}
+	else {
+	printf("b is smaller number");
+	}
+	return 0;
+	
+}
