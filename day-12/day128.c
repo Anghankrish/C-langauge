3d2df278b79b4334402c183b5d571df658f5925c
Anghankrish <134935345+Anghankrish@users.noreply.github.com>
2023-06-08 23:39:47 +0530

    Add files via upload

---
+++ day-12/day128.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int i=1;
+	int n,sum;
+	printef("enter the number :");
+	scanf("%d",&n);
+	
+	while(i<=n);{
+	sum=sum+i;
+	i++;
+	}
+	printf("%d\n",sum);
+	return 0;
+}
